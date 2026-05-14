0f45157ad3603edda0d51f3caf97bdc2e367cc77
GitHub Action <action@github.com>
2026-05-14 11:50:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/bymaskeli53/kotlindesignpatternscreator/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/bymaskeli53/kotlindesignpatternscreator/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.bymaskeli53.kotlindesignpatternscreator.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/bymaskeli53/kotlindesignpatternscreator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bymaskeli53/kotlindesignpatternscreator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bymaskeli53.kotlindesignpatternscreator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bymaskeli53.kotlindesignpatternscreator.MyBundle
+import com.github.bymaskeli53.kotlindesignpatternscreator.services.MyProjectService
 import javax.swing.JButton
 
 
